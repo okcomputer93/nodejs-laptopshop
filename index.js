7120a0b62e999b7a9fd0bbe14092f71ebed732bc
Omar J <okcomputer2393@gmail.com>
2020-10-28 02:08:49 -0600

    Implement single laptop view by id

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const http = require('http');
 const url = require('url');
 
 //__dirname = Home folder
+// It can be synchronus because it runs only once
 const json = fileSystem.readFileSync(`${__dirname}/data/data.json`, 'utf-8');
 //console.log(__dirname);
 const laptopData = JSON.parse(json);
@@ -19,11 +20,28 @@ const server = http.createServer((req, res) => {
     }
     else if (pathName === '/laptop' && id < laptopData.length) {
         res.writeHead(200, {'Content-type': 'text/html'});
-        res.end(`This is the LAPTOP page for laptop ${id}`);
+        
+        //This is asynchronus, so users don't wait until it finishes to read this file
+        fileSystem.readFile(`${__dirname}/templates/template-laptop.html`, 'utf-8', (err, data) => {
+            const laptop = laptopData[id];
+            let output = data.replace(/{%PRODUCTNAME%}/g, laptop.productName);
+            output = output.replace(/{%IMAGE%}/g, laptop.image);
+            output = output.replace(/{%SCREEN%}/g, laptop.screen);
+            output = output.replace(/{%CPU%}/g, laptop.cpu);
+            output = output.replace(/{%SCREEN%}/g, laptop.screen);
+            output = output.replace(/{%STORAGE%}/g, laptop.storgae);
+            output = output.replace(/{%RAM%}/g, laptop.ram);
+            output = output.replace(/{%DESCRIPTION%}/g, laptop.description);
+            output = output.replace(/{%PRICE%}/g, laptop.price);
+            res.end(output);
+        });
+    }
+    else {
+        res.writeHead(404, {'Content-type': 'text/html'});
+        res.end('Uh oh. That was unexpected.');
     }
 
-    res.writeHead(404, {'Content-type': 'text/html'});
-    res.end('Uh oh. That was unexpected.');
+    
 });
 
 server.listen(1337, '127.0.0.1', () => {
